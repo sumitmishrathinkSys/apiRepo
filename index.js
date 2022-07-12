1dac82e8eb4568303c81b44f6e4c0f76c063ea11
Sumit Mishra <mishra.sumit@thinksys.com>
2022-07-12 21:51:28 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// https://apicreateanddeploy.herokuapp.com/posts
+// https://apicreateanddeploy.herokuapp.com
+
+
+const express = require('express');
+const app = express();
+const posts = require('./posts')
+const PORT = process.env.PORT || 3000
+
+app.get('/posts', (req,res) => res.json(posts))
+
+app.get('/', (req,res) => {
+    res.send( 'Api is running.')
+})
+
+app.listen(PORT, () => console.log("Server is running"))
